4f632525ebf346282ada77729b94ac7d65127539
piyuxh12 <myhappyfamily1234@gmail.com>
2025-03-04 15:25:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function startDraw() {
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
 
+    if (name && phone) {
+        fetch('/lucky_draw', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name, phone })
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("result").innerText = `Congratulations ${name}! You won a ${data.prize}.`;
+        });
+    } else {
+        alert("Please enter your name and phone number.");
+    }
+}
